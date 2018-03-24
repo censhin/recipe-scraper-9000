0d4ad52546e50298a6b79e7d5e85077ef5dc9672
censhin <censhin@gmail.com>
2018-03-23 23:40:41 -0500

    feat(all): added ability to dynamically scrape for images, poorly

--- app/image_scraper.py
+++ app/image_scraper.py
@@ -5,26 +5,33 @@ from bs4 import BeautifulSoup
 
 
 allrecipes_pattern = r"https://images.media-allrecipes.com/userphotos/560x315/\d+\.jpg"
+foodnetwork_pattern = r"//food.fnr.sndimg.com/content/dam/images/food/fullset/.*\.jpeg"
+
 allrecipes_regex = re.compile(allrecipes_pattern)
+foodnetwork_regex = re.compile(foodnetwork_pattern)
 
 
-def find_image(url):
+def find_image(url, host, title):
     data = request.urlopen(url).read()
     soup = BeautifulSoup(data, "html.parser")
+    if host == "allrecipes.com":
+        return find_allrecipes_image(soup)
+    elif host == "foodnetwork.com":
+        return find_foodnetwork_image(soup, title)
+
+
+def find_allrecipes_image(soup):
     images = soup.findAll("img", {"src": allrecipes_regex})
     return images[0]["src"]
 
 
+def find_foodnetwork_image(soup, title):
+    images = soup.findAll("img", {"src": foodnetwork_regex})
+    for image in images:
+        if title in image.get("alt"):
+            return "https:{0}".format(image.get("src", ""))
+
+
 def download_image(filename, url):
-    directory = "images"
-    path = "{0}/{1}.jpg".format(directory, filename)
+    path = "{0}/{1}".format("images", filename)
     request.urlretrieve(url, path)
-
-
-def main():
-    image = find_image("https://www.allrecipes.com/recipe/8358/apple-cake-iv/")
-    download_image("test", image)
-
-
-if __name__ == "__main__":
-    main()
--- app/recipe_scraper.py
+++ app/recipe_scraper.py
@@ -3,6 +3,7 @@ import json
 from recipe_scrapers import scrape_me
 
 from image_scraper import download_image
+from image_scraper import find_image
 
 
 def get_recipes():
@@ -28,14 +29,24 @@ def recipe_to_dict(scrape):
     recipe["instructions"] = scrape.instructions()
     return recipe
 
+def get_file_type(image_url):
+    file_type = image_url[-4:]
+    print(file_type)
+    if "jpeg" == file_type:
+        return "jpeg"
+    elif ".jpg" == file_type:
+        return "jpg"
+    elif ".png" == file_type:
+        return "png"
 
-def get_filename(recipe_dict):
+
+def get_file_name(recipe_dict):
     return recipe_dict.get("title", "unknown").lower().replace(" ", "-")
 
 
 def write_to_file(recipe_dict):
     directory = "data"
-    filename = get_filename(recipe_dict)
+    filename = get_file_name(recipe_dict)
     path = "{0}/{1}.json".format(directory, filename)
     with open(path, "w") as f:
         f.write(json.dumps(recipe_dict))
@@ -47,7 +58,9 @@ def main():
         scrape = scrape_me(recipe)
         recipe_dict = recipe_to_dict(scrape)
         write_to_file(recipe_dict)
-        download_image(get_filename(recipe_dict))
+        image_url = find_image(recipe_dict.get("source"), scrape.host(), recipe_dict.get("title"))
+        filename = "{0}.{1}".format(get_file_name(recipe_dict), get_file_type(image_url))
+        download_image(filename, image_url)
 
 
 if __name__ == "__main__":
